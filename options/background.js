49d828bd36230ea8dd23dcf2ec202a41547e0cb0
Ryan <raberger1@gmail.com>
2025-06-16 19:03:00 -0400

    remove logs

--- options/background.js
+++ options/background.js
@@ -1,12 +1,9 @@
 chrome.runtime.onMessage.addListener((message) => {
     if (message.action === 'saveBlacklist') {
-        console.log('saving bl');
         return chrome.storage.local.set({ 'blacklist': message.content });
     } else if (message.action === 'saveWhitelist') {
-        console.log('saving wl');
         return chrome.storage.local.set({ 'whitelist': message.content });
     } else if (message.action === 'saveCustomList') {
-        console.log('saving cl');
         return chrome.storage.local.set({ 'customList': message.content });
     }
 });
